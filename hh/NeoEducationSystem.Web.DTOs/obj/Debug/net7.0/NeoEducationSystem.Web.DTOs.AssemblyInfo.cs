523985d442937deb0988c30e9ee9f0059e9960fb
tmkayy <96301516+tmkayy@users.noreply.github.com>
2024-01-13 13:10:25 +0200

    commit 8

--- hh/NeoEducationSystem.Web.DTOs/obj/Debug/net7.0/NeoEducationSystem.Web.DTOs.AssemblyInfo.cs
+++ hh/NeoEducationSystem.Web.DTOs/obj/Debug/net7.0/NeoEducationSystem.Web.DTOs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeoEducationSystem.Web.DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b61a634e25397fbbe109cb23bb152b416e878e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeoEducationSystem.Web.DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeoEducationSystem.Web.DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
