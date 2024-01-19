677c3e8b0064ca385feefc16016ac0d662dfcbce
tmkayy <96301516+tmkayy@users.noreply.github.com>
2024-01-19 18:02:29 +0200

    register pop up

--- hh/reactapp/src/components/LogInForm.js
+++ hh/reactapp/src/components/LogInForm.js
@@ -44,41 +44,41 @@ const LogInForm = () => {
 
   return (
     <div className="backgroundLR">
-    <div className="log-in-container">
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label htmlFor="email" className='LR-letters'>Email:</label>
-        <input className="LR-input-field"
-          type="text"
-          id="email"
-          name="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        {formik.touched.email && formik.errors.email && (
-          <div className='ER-letters'>{formik.errors.email}</div>
-        )}
+      <div className="log-in-container">
+        <form onSubmit={formik.handleSubmit}>
+          <div>
+            <label htmlFor="email" className='LR-letters'>Email:</label>
+            <input className="LR-input-field"
+              type="text"
+              id="email"
+              name="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email && (
+              <div className='ER-letters'>{formik.errors.email}</div>
+            )}
+          </div>
+          <div>
+            <label htmlFor="password" className='LR-letters' >Парола:</label>
+            <input className="LR-input-field"
+              type="password"
+              id="password"
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <div className='ER-letters'>{formik.errors.password}</div>
+            )}
+          </div>
+          <div>
+            <button type="submit" className="LR-submit">Влезни</button>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor="password" className='LR-letters' >Парола:</label>
-        <input className="LR-input-field"
-          type="password"
-          id="password"
-          name="password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-        />
-        {formik.touched.password && formik.errors.password && (
-          <div className='ER-letters'>{formik.errors.password}</div>
-        )}
-      </div>
-      <div>
-        <button type="submit" className = "LR-submit">Влезни</button>
-      </div>
-    </form>
-    </div>
     </div>
   );
 };
--- hh/reactapp/src/components/RegisterForm.js
+++ hh/reactapp/src/components/RegisterForm.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import axios from'axios';
+import axios from 'axios';
 import { API_URL } from '../common/GlobalConstants';
+import Swal from 'sweetalert2';
 
 const RegisterForm = () => {
     const formik = useFormik({
@@ -25,67 +26,76 @@ const RegisterForm = () => {
                 .required('Паролата е задължителна'),
         }),
         onSubmit: (values) => {
-           axios.post(API_URL + '/api/User/register', values)
-           .then(response => {
-            console.log('Response:', response.data);
-          })
-          .catch(error => {
-            console.error('Error:', error.message);
-          });
+            axios.post(API_URL + '/api/User/register', values)
+                .then(response => {
+                    console.log('Response:', response.data);
+                    Swal.fire({
+                        title: "Успешна регистрация!",
+                        text: "Можеш да влезеш в профила си.",
+                        backdrop: "rgba(0,0,0,0.8)",
+                        background: "#000000 url(https://i.gifer.com/QFNv.gif)",
+                        color: "#fff",
+                        iconColor: "#FF00FF",
+                        confirmButtonColor: "#ee4542"
+
+                    });
+                })
+                .catch(error => {
+                    console.error('Error:', error.message);
+                });
         },
     });
-
     return (
         <div className="backgroundLR">
-    <div className="log-in-container">
-        <form onSubmit={formik.handleSubmit}>
-            <div>
-                <label htmlFor="username" className='LR-letters'>Име:</label>
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.username}
-                />
-                {formik.touched.username && formik.errors.username && (
-                    <div className='ER-letters'>{formik.errors.username}</div>
-                )}
+            <div className="log-in-container">
+                <form onSubmit={formik.handleSubmit}>
+                    <div>
+                        <label htmlFor="username" className='LR-letters'>Име:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.username}
+                        />
+                        {formik.touched.username && formik.errors.username && (
+                            <div className='ER-letters'>{formik.errors.username}</div>
+                        )}
+                    </div>
+                    <div>
+                        <label htmlFor="email" className='LR-letters'>Email:</label>
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                        />
+                        {formik.touched.email && formik.errors.email && (
+                            <div className='ER-letters'>{formik.errors.email}</div>
+                        )}
+                    </div>
+                    <div>
+                        <label htmlFor="password" className='LR-letters'>Парола:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.password}
+                        />
+                        {formik.touched.password && formik.errors.password && (
+                            <div className='ER-letters'>{formik.errors.password}</div>
+                        )}
+                    </div>
+                    <div>
+                        <button type="submit">Регистрирай се</button>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label htmlFor="email" className='LR-letters'>Email:</label>
-                <input
-                    type="text"
-                    id="email"
-                    name="email"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.email}
-                />
-                {formik.touched.email && formik.errors.email && (
-                    <div className='ER-letters'>{formik.errors.email}</div>
-                )}
-            </div>
-            <div>
-                <label htmlFor="password" className='LR-letters'>Парола:</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.password}
-                />
-                {formik.touched.password && formik.errors.password && (
-                    <div className='ER-letters'>{formik.errors.password}</div>
-                )}
-            </div>
-            <div>
-                <button type="submit">Регистрирай се</button>
-            </div>
-        </form>
-        </div>
         </div>
     );
 };
