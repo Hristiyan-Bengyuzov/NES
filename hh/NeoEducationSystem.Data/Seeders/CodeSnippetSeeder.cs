1c6a01691bc541c95b2236efd073889caca36a2b
Hris <xrisko06@abv.bg>
2024-02-06 22:33:59 +0200

    seeded info about ifs and shit

--- hh/NeoEducationSystem.Data/Seeders/CodeSnippetSeeder.cs
+++ hh/NeoEducationSystem.Data/Seeders/CodeSnippetSeeder.cs
@@ -2,86 +2,116 @@
 
 namespace NeoEducationSystem.Data.Seeders
 {
-	internal class CodeSnippetSeeder : ISeeder
-	{
-		public async Task SeedAsync(NeoEducationDbContext context, IServiceProvider serviceProvider)
-		{
-			if (context.CodeSnippets.Any()) return;
+    internal class CodeSnippetSeeder : ISeeder
+    {
+        public async Task SeedAsync(NeoEducationDbContext context, IServiceProvider serviceProvider)
+        {
+            if (context.CodeSnippets.Any()) return;
 
-			await context.CodeSnippets.AddRangeAsync(new CodeSnippet[]
-			{
+            await context.CodeSnippets.AddRangeAsync(new CodeSnippet[]
+            {
 				// integral types
 				new CodeSnippet
-				{
-					ParagraphId = 2,
-					Language = "csharp",
-					Code = "int integerNumber = 42;\r\nlong longNumber = 1234567890;"
-				},
-				new CodeSnippet
-				{
-					ParagraphId = 2,
-					Language = "java",
-					Code = "int integerNumber = 42;\r\nlong longNumber = 1234567890L;"
-				},
+                {
+                    ParagraphId = 2,
+                    Language = "csharp",
+                    Code = "int integerNumber = 42;\r\nlong longNumber = 1234567890;"
+                },
+                new CodeSnippet
+                {
+                    ParagraphId = 2,
+                    Language = "java",
+                    Code = "int integerNumber = 42;\r\nlong longNumber = 1234567890L;"
+                },
 
 				// floating point
 				new CodeSnippet
-				{
-					ParagraphId = 4,
-					Language = "csharp",
-					Code = "double floatingPointNumber = 3.14;"
-				},
-				new CodeSnippet
-				{
-					ParagraphId = 4,
-					Language = "java",
-					Code = "double floatingPointNumber = 3.14;"
-				},
+                {
+                    ParagraphId = 4,
+                    Language = "csharp",
+                    Code = "double floatingPointNumber = 3.14;"
+                },
+                new CodeSnippet
+                {
+                    ParagraphId = 4,
+                    Language = "java",
+                    Code = "double floatingPointNumber = 3.14;"
+                },
 
 				// char
 				new CodeSnippet
-				{
-					ParagraphId = 5,
-					Language = "csharp",
-					Code = "char character = \'A\';",
-				},
-				new CodeSnippet
-				{
-					ParagraphId = 5,
-					Language = "java",
-					Code = "char character = \'A\';",
-				},
+                {
+                    ParagraphId = 5,
+                    Language = "csharp",
+                    Code = "char character = \'A\';",
+                },
+                new CodeSnippet
+                {
+                    ParagraphId = 5,
+                    Language = "java",
+                    Code = "char character = \'A\';",
+                },
 
 				// string
 				new CodeSnippet
-				{
-					ParagraphId = 6,
-					Language = "csharp",
-					Code = "string text = \"Hello, World!\";"
-				},
-				new CodeSnippet
-				{
-					ParagraphId = 6,
-					Language = "java",
-					Code = "String text = \"Hello, World!\";"
-				},
+                {
+                    ParagraphId = 6,
+                    Language = "csharp",
+                    Code = "string text = \"Hello, World!\";"
+                },
+                new CodeSnippet
+                {
+                    ParagraphId = 6,
+                    Language = "java",
+                    Code = "String text = \"Hello, World!\";"
+                },
 
 				// bool
 				new CodeSnippet
-				{
-					ParagraphId = 7,
-					Language = "csharp",
-					Code = "bool isTrue = true;"
-				},
-				new CodeSnippet
-				{
-					ParagraphId = 7,
-					Language = "java",
-					Code = "boolean isTrue = true;"
-				}
-			});
+                {
+                    ParagraphId = 7,
+                    Language = "csharp",
+                    Code = "bool isTrue = true;"
+                },
+                new CodeSnippet
+                {
+                    ParagraphId = 7,
+                    Language = "java",
+                    Code = "boolean isTrue = true;"
+                },
+				
 
-			await context.SaveChangesAsync();
-		}
-	}
+
+				// primer s if
+				new CodeSnippet
+                {
+                    ParagraphId = 9,
+                    Language = "csharp",
+                    Code = "int n = 18;\r\n\r\nif (n > 18)\r\n{\r\n    Console.WriteLine(\"Числото е по-голямо от 18.\");\r\n}\r\nelse\r\n{\r\n    Console.WriteLine(\"Числото е по-малко или равно на 18.\");\r\n}"
+                },
+                new CodeSnippet
+                {
+                    ParagraphId = 9,
+                    Language = "java",
+                    Code = "int n = 18;\r\n\r\nif (n > 18) {\r\n  System.out.println(\"Числото е по-голямо от 18.\");\r\n} else {\r\n  System.out.println(\"Числото е по-малко или равно на 18.\");\r\n}"
+                },
+				
+				// primer s switch
+				new CodeSnippet
+                {
+                    ParagraphId = 10,
+                    Language = "csharp",
+                    Code = "int weekDay = 3;\r\nstring day;\r\n\r\nswitch (weekDay)\r\n{\r\n    case 1:\r\n        day = \"Понеделник\";\r\n        break;\r\n    case 2:\r\n        day = \"Вторник\";\r\n        break;\r\n    case 3:\r\n        day = \"Сряда\";\r\n        break;\r\n    case 4:\r\n        day = \"Четвъртък\";\r\n        break;\r\n    case 5:\r\n        day = \"Петък\";\r\n        break;\r\n    case 6:\r\n        day = \"Събота\";\r\n        break;\r\n    case 7:\r\n        day = \"Неделя\";\r\n        break;\r\n    default:\r\n        day = \"Невалиден ден\";\r\n        break;\r\n}\r\n\r\nConsole.WriteLine($\"Денят от седмицата е: {day}\");"
+                },
+                new CodeSnippet
+                {
+                    ParagraphId = 10,
+                    Language = "java",
+                    Code = "int weekDay = 3;\r\nString day;\r\n\r\nswitch (weekDay) {\r\n    case 1:\r\n        day = \"Понеделник\";\r\n        break;\r\n    case 2:\r\n        day = \"Вторник\";\r\n        break;\r\n    case 3:\r\n        day = \"Сряда\";\r\n        break;\r\n    case 4:\r\n        day = \"Четвъртък\";\r\n        break;\r\n    case 5:\r\n        day = \"Петък\";\r\n        break;\r\n    case 6:\r\n        day = \"Събота\";\r\n        break;\r\n    case 7:\r\n        day = \"Неделя\";\r\n        break;\r\n    default:\r\n        day = \"Невалиден ден\";\r\n        break;\r\n    }\r\n\r\nSystem.out.println(\"Денят от седмицата е: \" + day);"
+                }
+            }); ;
+
+            await context.SaveChangesAsync();
+        }
+    }
 }
--- hh/NeoEducationSystem.Data/Seeders/ParagraphSeeder.cs
+++ hh/NeoEducationSystem.Data/Seeders/ParagraphSeeder.cs
@@ -2,54 +2,71 @@
 
 namespace NeoEducationSystem.Data.Seeders
 {
-	public class ParagraphSeeder : ISeeder
-	{
-		public async Task SeedAsync(NeoEducationDbContext context, IServiceProvider serviceProvider)
-		{
-			if (context.Paragraphs.Any()) return;
+    public class ParagraphSeeder : ISeeder
+    {
+        public async Task SeedAsync(NeoEducationDbContext context, IServiceProvider serviceProvider)
+        {
+            if (context.Paragraphs.Any()) return;
 
-			await context.Paragraphs.AddRangeAsync(new Paragraph[]
-			{
+            await context.Paragraphs.AddRangeAsync(new Paragraph[]
+            {
 				// main data types
 				new Paragraph
-				{
-					LessonId = 1,
-					Content = "В програмирането основните типове данни са основните строителни блокове за съхранение и обработка на информация. В C# и Java има различни типове данни, които могат да бъдат разделени в няколко основни категории."
-				},
-				new Paragraph
-				{
-					LessonId = 1,
-					Content = "Целочислените типове данни се използват за съхранение на цели числа без десетична запетая. Примери в C# и Java:"
-				},
-				new Paragraph
-				{
-					LessonId = 1,
-					Content = "Тези типове имат различен обхват. int е от -2^31 до 2^31. long е от -2^63 до 2^63."
-				},
-				new Paragraph
-				{
-					LessonId = 1,
-					Content = "Съхранението на дробни числа става чрез типът double. Неговият обхват е от -1.7*10^308 до 1.7*10^308. Примери:"
-				},
-				new Paragraph
-				{
-					LessonId = 1,
-					Content = "Символните типове данни се използват за съхранение на единични символи. Те се пишат с единични кавички."
-				},
-				new Paragraph
-				{
-					LessonId = 1,
-					Content = "Низовете са типове данни, които се използват за съхранение на последователност от символи. Пишат се с двойни кавички."
-				},
-				new Paragraph
-				{
-					LessonId = 1,
-					Content = "Булевите типове данни се използват за съхранение на стойности true или false. Примери:"
-				}
+                {
+                    LessonId = 1,
+                    Content = "В програмирането основните типове данни са основните строителни блокове за съхранение и обработка на информация. В C# и Java има различни типове данни, които могат да бъдат разделени в няколко основни категории."
+                },
+                new Paragraph
+                {
+                    LessonId = 1,
+                    Content = "Целочислените типове данни се използват за съхранение на цели числа без десетична запетая. Примери в C# и Java:"
+                },
+                new Paragraph
+                {
+                    LessonId = 1,
+                    Content = "Тези типове имат различен обхват. int е от -2^31 до 2^31. long е от -2^63 до 2^63."
+                },
+                new Paragraph
+                {
+                    LessonId = 1,
+                    Content = "Съхранението на дробни числа става чрез типът double. Неговият обхват е от -1.7*10^308 до 1.7*10^308. Примери:"
+                },
+                new Paragraph
+                {
+                    LessonId = 1,
+                    Content = "Символните типове данни се използват за съхранение на единични символи. Те се пишат с единични кавички."
+                },
+                new Paragraph
+                {
+                    LessonId = 1,
+                    Content = "Низовете са типове данни, които се използват за съхранение на последователност от символи. Пишат се с двойни кавички."
+                },
+                new Paragraph
+                {
+                    LessonId = 1,
+                    Content = "Булевите типове данни се използват за съхранение на стойности true или false. Примери:"
+                },
 
-			});
 
-			await context.SaveChangesAsync();
-		}
-	}
+                // conditional statements
+                new Paragraph
+                {
+                    LessonId = 2,
+                    Content = "Условните конструкции или проверки са парчета код, чрез който изпълняваме различни действия в зависимост от някакво условие. Условието е логически израз, т.е. израз, който може да приема стойност истина или лъжа. Има различни условни оператори за постигане на това. Примери са if, else if, else и switch.",
+                },
+                new Paragraph
+                {
+                    LessonId = 2,
+                    Content = "Пресмята се стойността на условието и ако то е вярно се изпълнява if, иначе се изпълнява else. Пример:"
+                },
+                new Paragraph
+                {
+                    LessonId = 2,
+                    Content = "switch конструкцията позволява проверка на различни стойности на променлива и изпълнява код в зависимост от нея. Различните случаи се описват в case, а ако нито едно от условията не се изпълни, се изпълнява default блокът. Пример:"
+                }
+            });
+
+            await context.SaveChangesAsync();
+        }
+    }
 }
